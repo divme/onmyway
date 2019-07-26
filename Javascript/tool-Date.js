6b1fe4d62c12f36cbe42f2b763495d5d02e557b9
divme <979973510@qq.com>
2019-07-26 18:24:05 +0800

    tool func update

--- Javascript/tool-Date.js
+++ Javascript/tool-Date.js
@@ -1,13 +1,20 @@
-// 格式化日期： 年月日  时分秒
-// 参数： 第一个可以接受的日期格式，第二个是否只有日期，第三个日期连接符，第四个时间连接符
-function formatDate(time, onlyDate, symbol1, symbol2) {
-    symbol1 = symbol1 || '-'
-    symbol2 = symbol2 || ':'
+/*
+* created by zy on 2019.07.26
+*/
+
+// 可以选择：年月日 时分秒 or 年月日 or 时分秒
+// option = {
+//     onlyDate: true/false,
+//     onlyTime: true/false,
+//     dateSymbol: '--',
+//     timeSymbol: ':'
+// }
+function formatTime(time, option) {
+    option = option? option : {}
+    var dateSymbol = option.dateSymbol || '-'
+    var timeSymbol = option.timeSymbol || ':'
     if (time) {
         var date = new Date(time)
-        /* 在日期格式中，月份是从0开始的，因此要加1
-         * 使用三元表达式在小于10的前面加0，以达到格式统一  如 09:11:05
-         * */
         var year = date.getFullYear()
         var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
         var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
@@ -18,15 +25,52 @@ function formatDate(time, onlyDate, symbol1, symbol2) {
             return '--'
         }
         // 拼接
-        if (onlyDate) {
-            return year + symbol1 + month + symbol1 + day
+        if (option.onlyDate) {
+            return year + dateSymbol + month + dateSymbol + day
         }
-        return year + symbol1 + month + symbol1 + day + ' ' + hours + symbol2 + minutes + symbol2 + seconds
+        if (option.onlyTime) {
+            return hours + timeSymbol + minutes + timeSymbol + seconds
+        }
+        return year + dateSymbol + month + dateSymbol + day + ' ' + hours + timeSymbol + minutes + timeSymbol + seconds
     }
     return ''
 }
-// 格式化时间：时：分：秒
-function formatTime(time, symbol) {
+// 格式化时间：年月日，时分秒
+function formatDate(time, dateSymbol, timeSymbol) {
+    dateSymbol = option.dateSymbol || '-'
+    timeSymbol = option.timeSymbol || ':'
+    if (time) {
+        var date = new Date(time)
+        var year = date.getFullYear()
+        var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+        var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+        var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+        var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+        var seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+        if (isNaN(year) || isNaN(month) || isNaN(day) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
+            return '--'
+        }
+        return year + dateSymbol + month + dateSymbol + day + ' ' + hours + timeSymbol + minutes + timeSymbol + seconds
+    }
+    return ''
+}
+// 格式化时间：年月日
+function formatYmd(time, symbol) {
+    symbol = symbol || '-'
+    if (time) {
+        var date = new Date(time)
+        var year = date.getFullYear()
+        var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+        var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+        if (isNaN(year) || isNaN(month) || isNaN(day)) {
+            return '--'
+        }
+        return year + symbol + month + symbol + day
+    }
+    return ''
+}
+// 格式化时间：时分秒
+function formatHms(time, symbol) {
     symbol = symbol || ':'
     if (time) {
         var date = new Date(time)
--- Javascript/tool-Number.js
+++ Javascript/tool-Number.js
@@ -1,92 +1,10 @@
 /**
  * Created by Administrator on 2019/7/21 0021.
  */
-
-// 两个数字比较是否相等
-function ifEqual(n, m){
-    return Math.abs(n - m) < Number.EPSILON
-}
-
-// 格式化金额
-// 数字类处理函数，支持 12.74 和 1,238,12.55 两种格式
-// 保留几位小数
-// todo
-function keepDecimals(num, n) {
-    if (String(num).indexOf(',') > -1) {
-        num = num.split(',').join('')
-        if (isNaN(Number(num))) {
-            return '--'
-        }
-    }
-    if (isNaN(num) || num === null || num === '') {
-        return '--'
-    }
-    n = (n || n === 0) ? n : 2
-    return Number(num).toFixed(n)
-}
-// 小数转化为百分比
-function toPercent(num, n) {
-    if (String(num).indexOf(',') > -1) {
-        num = num.split(',').join('')
-        if (isNaN(Number(num))) {
-            return '--'
-        }
-    }
-    if (isNaN(num) || num === null || num === '') {
-        return '--'
-    }
-    n = (n || n === 0) ? n : 2
-    return Number(num * 100).toFixed(n) + '%'
-}
-// todo
-// 百分比数字转化为小数
-function toDecimals(num, n) {
-    if (String(num).indexOf(',') > -1) {
-        num = num.split(',').join('')
-        if (isNaN(Number(num))) {
-            return '--'
-        }
-    }
-    if (isNaN(num) || num === null || num === '') {
-        return '--'
-    }
-    n = (n || n === 0) ? n : 4
-    return Number(num / 100).toFixed(n)
-}
-// 小数加法
-// todo
-function addition(n, m) {
-    if (isNaN(n) || isNaN(m) || n === null || m === null || n === '' || m === '') {
-        return '--'
-    }
-    return Number(n) + Number(m)
-}
-
-// 数字格式化为千分位逗号格式
-// 参数：数字字符串
-function formatNumber(num) {
-    // 在table中不是数字，自动写入 '--'
-    if (isNaN(num) || num === null || num === '') {
-        return '--'
-    }
-    const arr = String(num).split('.')
-    if (arr[0].length > 3) {
-        arr[0] = arr[0].replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,')
-        num = arr.join('.')
-        return num
-    } else {
-        return num
-    }
-}
-// 千分位逗号格式，解析为正常数字,
-// 参数：数字字符串
-function parseNumber(str) {
-    const num = String(str).split(',').join('')
-    if (!isNaN(num) && num !== '') {
-        return num
-    }
-    return str
-}
+// 小数--> 百分比
+// 百分比--> 小数
+// 数字--> 金额格式
+// 金额格式--> 数字
 
 // 判断是否为数字
 function isNumber(num) {
@@ -96,9 +14,48 @@ function isNumber(num) {
     return !isNaN(Number(num))
 }
 
+// 小数 ==> 百分比
+function toPercent(num, n) {
+    if (isNaN(num) || num === '' || num === false || num === null) {
+        return '--'
+    }
+    n = (n || n === 0) ? n : 2
+    return Number(num * 100).toFixed(n) + '%'
+}
+// 百分比 ==> 小数
+function toDecimals(num, n) {
+    num = String(num).indexOf('%') === String(num).length - 1 ? num.slice(0, -1) :num
+    if (isNaN(num) || num === '' || num === false || num === null) {
+        return '--'
+    }
+    n = (n || n === 0) ? n : 2
+    return Number(num / 100).toFixed(n)
+}
+
+
+// 数字 ==> 千分位逗号格式
+// 参数：数字字符串
+function formatNumber(num) {
+    if (isNaN(num) || num === '' || num === false || num === null) {
+        return '--'
+    }
+    // 正向肯定预查：确保匹配项后面位数是3的倍数
+    // 反向否定预查: 确保小数点后的位数不会被格式化
+    return String(num).replace(/(?<!\d+\.\d*)\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,')
+    // return String(num).replace(/(?<=(\d|,)+)\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,')
+}
+// 千分位逗号格式 ==> 正常数字,
+// 参数：数字字符串
+function parseNumber(str) {
+    const num = String(str).split(',').join('')
+    if (!isNaN(num) && num !== '') {
+        return num
+    }
+    return str
+}
+
 /**
  * 将数值四舍五入(保留2位小数)后格式化成金额形式
- *
  * @param num 数值(Number或者String)
  * @return String --金额格式的字符串,如'1,234,567.45'
  * @type
@@ -134,32 +91,6 @@ function formatMoney(num, decimal) {
 }
 
 // -------------------------------------------------计算精度问题--------------------------------------------
-//除法函数，用来得到精确的除法结果
-//说明：javascript的除法结果会有误差，在两个浮点数相除的时候会比较明显。这个函数返回较为精确的除法结果。
-//调用：accDiv(arg1,arg2)
-//返回值：arg1除以arg2的精确结果
-function div(arg1,arg2){
-    var t1 = 0, t2 = 0, r1, r2;
-    try {t1 = arg1.toString().split(".")[1].length} catch(e) { t1 = 0}
-    try {t2 = arg2.toString().split(".")[1].length} catch(e) { t2 = 0}
-    r1 = Number(arg1.toString().replace(".",""))
-    r2 = Number(arg2.toString().replace(".",""))
-    return (r1/r2)*Math.pow(10,t2-t1);
-}
-
-
-//乘法函数，用来得到精确的乘法结果
-//说明：javascript的乘法结果会有误差，在两个浮点数相乘的时候会比较明显。这个函数返回较为精确的乘法结果。
-//调用：mul(arg1,arg2)
-//返回值：arg1乘以arg2的精确结果
-function mul(arg1,arg2)
-{
-    var m=0,s1=arg1.toString(),s2=arg2.toString();
-    try{m+=s1.split(".")[1].length}catch(e){}
-    try{m+=s2.split(".")[1].length}catch(e){}
-    return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)
-}
-
 // 加法
 function add(arg1,arg2){
     var r1,r2,m;
@@ -169,7 +100,7 @@ function add(arg1,arg2){
     return (arg1*m+arg2*m)/m
 }
 
-// 减法：
+// 减法
 function subtr(arg1,arg2){
     var r1,r2,m,n;
     try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}
@@ -180,3 +111,23 @@ function subtr(arg1,arg2){
     n=(r1>=r2)?r1:r2;
     return ((arg1*m-arg2*m)/m).toFixed(n);
 }
+// 乘法
+function mul(arg1,arg2){
+    var m=0,s1=arg1.toString(),s2=arg2.toString();
+    try { m+=s1.split(".")[1].length }catch(e){}
+    try { m+=s2.split(".")[1].length }catch(e){}
+    return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)
+}
+//除法
+function div(arg1, arg2){
+    var t1 = 0, t2 = 0, r1, r2;
+    try { t1 = arg1.toString().split(".")[1].length } catch(e) { t1 = 0}
+    try { t2 = arg2.toString().split(".")[1].length } catch(e) { t2 = 0}
+    r1 = Number(arg1.toString().replace(".",""))
+    r2 = Number(arg2.toString().replace(".",""))
+    return (r1/r2)*Math.pow(10,t2-t1);
+}
+
+
+
+
