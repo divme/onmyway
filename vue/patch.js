afda88e33dfd4c42c4517d5ea15b3936f2d3f005
zongqilin <328560150@qq.com>
2020-10-22 21:06:37 +0800

    update toy-vue

---
+++ vue/patch.js
@@ -0,0 +1,21 @@
+const nodeOps = {
+    parentNode() {
+
+    },
+    insertBefore() {
+
+    },
+    removeChild() {
+
+    },
+    nextSibling() {
+
+    }
+}
+
+function insert(parent, ele, ref) {
+
+}
+function createEle() {
+
+}
